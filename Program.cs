4bb9d0a28bda823d7112cbdc0d61b84d0459cd03
russelibale808 <russelibale808@gmail.com>
2022-10-20 04:20:30 -1000

    Selection Statment Exercise 2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Switching Case Exsercise
+
+            Console.WriteLine("What is your favorite school subject");
+            string myFavSchoolSub  = Console.ReadLine();
+
+           
+            switch (myFavSchoolSub)
+            {
+                case "math":
+                    Console.WriteLine("Math can be tricky");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("Social studies has alot of history and geography");
+                    break;
+                case "english":
+                    Console.WriteLine("Enligh is fun");
+                    break;
+                case "wood class":
+                    Console.WriteLine("I love wood class!");
+                    break;
+                case "arts and crafts":
+                    Console.WriteLine("I love doing arts and crafts");
+                    break;
+                default:
+                    Console.WriteLine("I don't know what subject that is");
+                    break;
+            }
         }
     }
 }
